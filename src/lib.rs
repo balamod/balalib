25e4485adb3a982c0dda2e4c9c40ed9b6a8b42d5
UwU <mmimi0593@gmail.com>
2024-05-21 14:36:11 +0200

    Fixed export table

--- src/lib.rs
+++ src/lib.rs
@@ -16,12 +16,10 @@ fn echo(_: &Lua, name: String) -> LuaResult<String> {
 #[mlua::lua_module]
 fn balalib(lua: &Lua) -> LuaResult<LuaTable> {
     let exports = lua.create_table()?;
-    let balalib_table = lua.create_table()?;
-    balalib_table.set("echo", lua.create_function(echo)?)?;
-    balalib_table.set("fetch_mods", lua.create_function(|lua, ()| fetch_mods(lua, ()))?)?;
-    balalib_table.set("get_local_mods", lua.create_function(|lua, ()| get_local_mods(lua, ()))?)?;
-    balalib_table.set("need_update", lua.create_function(|lua, ()| need_update(lua, ()))?)?;
-    balalib_table.set("version", VERSION)?;
-    exports.set("balalib", balalib_table)?;
+    exports.set("echo", lua.create_function(echo)?)?;
+    exports.set("fetch_mods", lua.create_function(|lua, ()| fetch_mods(lua, ()))?)?;
+    exports.set("get_local_mods", lua.create_function(|lua, ()| get_local_mods(lua, ()))?)?;
+    exports.set("need_update", lua.create_function(|lua, ()| need_update(lua, ()))?)?;
+    exports.set("version", VERSION)?;
     Ok(exports)
 }
