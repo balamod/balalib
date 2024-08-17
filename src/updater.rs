c4855413288d10808173b1d9c11523365dd9c032
UwU <mmimi0593@gmail.com>
2024-08-17 22:56:50 +0200

    linux + macos self update (balamod)

--- src/updater.rs
+++ src/updater.rs
@@ -1,3 +1,6 @@
+use std::os::unix::fs::PermissionsExt;
+use std::thread::sleep;
+use std::time::Duration;
 use crate::core::restart;
 use mlua::prelude::LuaResult;
 
@@ -33,7 +36,7 @@ pub fn need_update(current_version: String) -> LuaResult<bool> {
 }
 
 #[cfg(target_os = "windows")]
-pub fn self_update(cli_ver: &str) -> LuaResult<()> {
+pub fn self_update_balamod(cli_ver: &str) -> LuaResult<()> {
     let url = format!(
         "https://github.com/balamod/balamod/releases/download/{}/balamod-{}-windows.exe",
         cli_ver, cli_ver
@@ -42,13 +45,74 @@ pub fn self_update(cli_ver: &str) -> LuaResult<()> {
         .user_agent("balalib")
         .build()
         .unwrap();
+
     let mut response = client.get(&url).send().unwrap();
-    let mut file = std::fs::File::create("balamod.exe").unwrap();
+    let mut file = std::fs::File::create("balamod.tmp").unwrap();
     std::io::copy(&mut response, &mut file).unwrap();
+    std::fs::set_permissions("balamod.tmp", std::fs::Permissions::from_mode(0o755))?;
+    std::fs::rename("balamod.tmp", "balamod")?;
+    drop(file);
+
+    let output = std::process::Command::new("balamod")
+        .arg("-u")
+        .arg("-a")
+        .output()?;
+
+    println!("{:?}", output);
+
     restart()
 }
 
 #[cfg(any(target_os = "macos", target_os = "linux"))]
-pub fn self_update(_cli_ver: &str) -> LuaResult<()> {
+pub fn self_update_balamod(cli_ver: &str) -> LuaResult<()> {
+    let mut filename = format!("balamod-{}-", cli_ver);
+    if cfg!(target_os = "macos") {
+        filename.push_str("mac.pkg");
+    } else {
+        filename.push_str("linux");
+    }
+
+    let url = format!("https://github.com/balamod/balamod/releases/download/{}/{}", cli_ver, filename);
+    let client = reqwest::blocking::Client::builder()
+        .user_agent("balalib")
+        .build()
+        .unwrap();
+
+    let mut response = client.get(&url).send().unwrap();
+
+    println!("Got response");
+
+    let mut file = std::fs::File::create("balamod.tmp").unwrap();
+    println!("Created file");
+    std::io::copy(&mut response, &mut file).unwrap();
+    println!("Copied response to file");
+    std::fs::set_permissions("balamod.tmp", std::fs::Permissions::from_mode(0o755))?;
+    println!("Set permissions");
+
+    // Move the temp file to the final name atomically
+    std::fs::rename("balamod.tmp", "balamod")?;
+    println!("Renamed file");
+
+    // Ensure the previous instance is completely terminated
+    sleep(Duration::from_secs(1));
+
+    //close file
+    drop(file);
+
+    if cfg!(target_os = "macos") {
+        let output = std::process::Command::new("./balamod")
+            .arg("-u")
+            .arg("-a")
+            .output()?;
+        println!("{:?}", output);
+    } else {
+        let output = std::process::Command::new("./balamod")
+            .arg("-u")
+            .arg("-a")
+            .arg("--linux-native")
+            .output()?;
+        println!("{:?}", output);
+    }
+
     restart()
 }
